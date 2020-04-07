85ac0ab58284f6dee3e3856a384cb9a53b6890f2
Andrew Gunter <andrewdavidgunter@gmail.com>
2020-04-07 09:09:03 -0400

    Implemented JNZ

--- src/emulatorShell.c
+++ src/emulatorShell.c
@@ -941,8 +941,13 @@ void executeInstruction(uint8_t opcode, uint8_t *operands, State8080 *state)
             state->pc += instructionSizes[opcode];
             break;
         case 0xC2: 
-            printInstructionInfo(opcode);
-            state->pc += instructionSizes[opcode];
+            // JNZ addr
+            // if NZ, PC = addr
+            if(!state->flags.zero){
+                state->pc = orderedOperands;
+			}else{
+                 state->pc += 3;
+			}
             break;
         case 0xC3: 
             // JMP adr - JUMP
