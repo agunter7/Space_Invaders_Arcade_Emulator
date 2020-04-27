f0b456eecf708716757c5eb2d61ae94c6bacf634
Andrew Gunter <andrewdavidgunter@gmail.com>
2020-04-27 10:51:44 -0400

    Hooked up VRAM to SDL graphics. Game now boots to initial screen, correctly displayed

--- src/arcadeMachine.c
+++ src/arcadeMachine.c
@@ -41,26 +41,26 @@ void playSpaceInvaders(ArcadeState *arcade)
         //Start timer
 
         quitGame = handleGameEvents(arcade);
-        logger("Handled game events\n");
+        //logger("Handled game events\n");
 
         // Clear screen
         SDL_RenderClear(arcade->renderer);
-        logger("Cleared screen\n");
+        //logger("Cleared screen\n");
 
         // Load/render window image
         SDL_UpdateTexture(texture, NULL, getCurrentFramePixels(arcade->cpu), SCREEN_WIDTH_PIXELS*BYTES_PER_PIXEL);
-        logger("Updated texture\n");
+        //logger("Updated texture\n");
         SDL_RenderCopy(arcade->renderer, texture, NULL, NULL);
-        logger("Copied texture to renderer\n");
+        //logger("Copied texture to renderer\n");
 
         // Play any sounds
 
         // Update screen
         SDL_RenderPresent(arcade->renderer);
-        logger("Presented renderer\n");
+        //logger("Presented renderer\n");
 
         // If frame time < (1/60)s, then stall
-        logger("Finished a main loop\n");
+        //logger("Finished a main loop\n");
     }
 }
 
@@ -111,7 +111,7 @@ uint32_t *getCurrentFramePixels(State8080 *cpu)
 {
     // get rotated pixel data from cpu
     // 1 bit per pixel
-    uint8_t *counterClockwisePixels = getVideoRAM(cpu);
+    uint8_t *rotatedPixels = getVideoRAM(cpu);
 
     // At this point, a byte contains data for 8 pixels. However, the order of the bytes is counter-clockwise.
     // This is because the original Space Invaders cabinet used a rotated CRT, so the developers accounted for this.
@@ -135,7 +135,7 @@ uint32_t *getCurrentFramePixels(State8080 *cpu)
     // Fixing problem #1 is easy, we just expand each bit to 32 bits and we can even add RGBA info as we desire .
     // Fixing problem #2 is messy. If we read from our data as currently ordered, we would have the pixel that should
     // be in the bottom-leftmost position ending up rendered in the top-leftmost position.
-    // To fix this, we can create a map from the corrected pixel indices to the rotated ones that we have now:
+    // To fix this, we can create a map from the render-order pixel indices to the rotated ones that we have now:
     /*
       0  : 255 -  1  : 511 -    ...   - 223 :57343
          |     -     |     -     |    -     |
@@ -161,10 +161,36 @@ uint32_t *getCurrentFramePixels(State8080 *cpu)
     // expand pixel bits
     // each bit from cpu will become 32 bits (RGBA format)
     // 8 bits for each of: red, green, blue, alpha
-    unsigned int numPixelBytes = SCREEN_HEIGHT_PIXELS*SCREEN_WIDTH_PIXELS*BYTES_PER_PIXEL;
+    unsigned int numPixels = SCREEN_HEIGHT_PIXELS*SCREEN_WIDTH_PIXELS;
+    unsigned int numPixelBytes = numPixels*BYTES_PER_PIXEL;
     uint32_t *currentFramePixels = malloc(numPixelBytes);
-    for(unsigned int i=0; i < numPixelBytes; i++){
+    unsigned int I_1 = 0;
+    unsigned int W = SCREEN_WIDTH_PIXELS;
+    unsigned int H = SCREEN_HEIGHT_PIXELS;
+    uint8_t currentByte = 0x00;
+    bool currentPixelBit = 0;
+    unsigned int byteIndex = 0;
+    unsigned int bitIndexWithinByte = 0;  // MSB index == 7, LSB index == 1
+    // Iterate through the pixels to be rendered on screen.
+    // top-left pixel = index 0, top-right = index 223, bottom-right = 57,343
+    for(unsigned int I_2 = 0; I_2 < numPixels; I_2++){
+        // Get the rotated index for the desired bit in VRAM that corresponds with the current pixel
+        I_1 = ((I_2%W)+1)*H - (floor(I_2/W)+1);
 
+        // Get the byte that contains the desired bit
+        byteIndex = floor(I_1 / 8);
+        currentByte = rotatedPixels[byteIndex];
+
+        // Get desired bit
+        bitIndexWithinByte = I_1 % 8;
+        currentPixelBit = (currentByte >> bitIndexWithinByte) & 0x01;
+
+        // Expand bit to 32 bits RGBA info
+        if(currentPixelBit == 1){
+            currentFramePixels[I_2] = 0xffffffff;
+        }else{
+            currentFramePixels[I_2] = 0x00000000;
+        }
     }
 
     // return
